42856cf59b495c1d34e0ae09e8c7bb13d5fe06f7
AGBO MERSHACK KWESI <goodvision99@outlook.com>
2022-10-26 08:57:07 +0000

    further update

--- Route/Route.js
+++ Route/Route.js
@@ -6,9 +6,63 @@ Router.get('/questions_field_resultScores', (req, res, next) => {
   res.render('result')
 })
 
+if ((url = "/questionPortal/mathematics")) {
+   try {
+    
 Router.get('/questionPortal/mathematics', (req, res, next) => {
-    res.render('Ques',{layout:'Maths'});
+    res.render('math',{layout:'Maths'});
 })
+   } catch (error) {
+    res.send(error)
+   }
+ }
+if ((url = "/questionPortal/physics")) {
+  try {
+    Router.get("/questionPortal/physics", (req, res, next) => {
+      res.render("phys", { layout: "Phys" });
+    });
+  } catch (error) {
+    res.send(error);
+  }
+}
+if ((url = "/questionPortal/chemistry")) {
+  try {
+    Router.get("/questionPortal/chemistry", (req, res, next) => {
+      res.render("chemis", { layout: "Chems" });
+    });
+  } catch (error) {
+    res.send(error);
+  }
+}
+if ((url = "/questionPortal/economics")) {
+  try {
+    Router.get("/questionPortal/economics", (req, res, next) => {
+      res.render("econs", { layout: "Econs" });
+    });
+  } catch (error) {
+    res.send(error);
+  }
+}
+if ((url = "/questionPortal/science")) {
+   try {
+    
+Router.get("/questionPortal/science", (req, res, next) => {
+  res.render("sci", { layout: "Sci" });
+});
+   } catch (error) {
+    res.send(error)
+   }
+ }
+if ((url = "/questionPortal/geography")) {
+   try {
+    
+Router.get('/questionPortal/geography', (req, res, next) => {
+    res.render('geo',{layout:'Geo'});
+})
+   } catch (error) {
+    res.send(error)
+   }
+ }
 
 
 
